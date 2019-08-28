c3da7a119a57b8fa0e202449b864510ab18ee7a7
dani <dani.zaghian@gmail.com>
2019-08-28 12:03:29 -0700

    adds all products url and individual products urls

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,23 @@
 class Api::ProductsController < ApplicationController
+
+  def all_products_action
+    @products = Product.all
+    render "all_products.json.jb"
+  end
+
+  def first_product_action
+    @product = Product.first
+    render "first_product.json.jb"
+  end
+
+  def second_product_action
+    @product = Product.find_by(id: 2)
+    render "second_product.json.jb"
+  end
+
+  def third_product_action
+    @product = Product.find_by(id: 3)
+    render "second_product.json.jb"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/all_products_url" => "products#all_products_action"
+    get "/first_product_url" => "products#first_product_action"
+    get "/second_product_url" => "products#second_product_action"
+    get "/third_product_url" => "products#third_product_action"
+  end
 end
