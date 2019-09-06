ecc1e9402ca2ca5689350d560b398bbd67c58345
dani <dani.zaghian@gmail.com>
2019-09-06 13:26:35 -0700

    adds supplier and products methods

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,2 +1,9 @@
 class Supplier < ApplicationRecord
+  
+  # supplier has many products
+  def products
+    # return an ARRAY (collection) of products that belong to THIS supplier
+    Product.where(supplier_id: id)
+  end
+
 end
