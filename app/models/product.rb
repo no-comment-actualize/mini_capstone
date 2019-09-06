a7f481ab60d6cef4c094a0ab54618882e9b65413
dani <dani.zaghian@gmail.com>
2019-09-06 12:31:58 -0700

    adds supplier model method

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,13 @@ class Product < ApplicationRecord
   validates :price, numericality: { greater_than: 0 }
   validates :description, length: { in: 10..500 }
 
+  # product belongs to a supplier
+  def supplier
+    # return the supplier that THIS (self) product bleongs to
+    Supplier.find_by(id: supplier_id)
+    # returns a single supplier hash whos id matches THIS product's supplier_id
+  end
+
   def is_discounted?
     price <= 10
   end
