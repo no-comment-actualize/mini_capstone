4b5272d869f04c4f636aa6c2fcc55f82c6ea0bf4
dani <dani.zaghian@gmail.com>
2019-09-06 12:01:21 -0700

    adds supplier model and adds supplier_id foreign key to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_03_190648) do
+ActiveRecord::Schema.define(version: 2019_09_06_185154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2019_09_03_190648) do
     t.decimal "price", precision: 6, scale: 2
     t.string "image_url"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "supplier_id"
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone_number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
