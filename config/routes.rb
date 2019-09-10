f78369ba4a61b23cdc47b9fa97e3a73d3a883ec5
dani <dani.zaghian@gmail.com>
2019-09-10 09:20:53 -0700

    adds authentication

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
     post "/products" => "products#create"
     patch "/products/:id" => "products#update"
     delete "/products/:id" => "products#destroy"
+    post "/users" => "users#create"
+    post "/sessions" => "sessions#create"
   end
 end
